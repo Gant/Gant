3547e587d620f36d470740bea156afd5ac508c51
russel <russel@d02f77da-47a5-474d-b610-f18e4bea325a>
2008-01-02 19:02:41 +0000

    r8995@balin:  russel | 2008-01-02 18:37:30 +0000  First cut at a Gant Ant task.
    git-svn-id: https://svn.codehaus.org/gant/gant/trunk@8890 d02f77da-47a5-474d-b610-f18e4bea325a

---
+++ src/main/groovy/org/codehaus/gant/ant/Gant.java
@@ -0,0 +1,68 @@
+//  Gant -- A Groovy build framework based on scripting Ant tasks.
+//
+//  Copyright © 2008 Russel Winder
+//
+//  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in
+//  compliance with the License. You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software distributed under the License is
+//  distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+//  implied. See the License for the specific language governing permissions and limitations under the
+//  License.
+
+package org.codehaus.gant.ant ;
+
+import java.io.File ;
+
+import java.util.ArrayList ;
+
+import org.apache.tools.ant.BuildException ;
+import org.apache.tools.ant.Task ;
+
+import org.apache.tools.ant.types.Path ;
+import org.apache.tools.ant.types.Reference ;
+
+/**
+ *  Execute a Gant script.
+ *
+ * <p>This Ant task provides a Gant capability for continuous integration systems that do not directly
+ * support Gant but only Ant.</p>
+ *
+ * @author Russel Winder
+ */
+public class Gant extends Task {
+  /**
+   * Gantfile to load, default is build.gant.
+   */
+  private String file = "build.gant" ;
+  /**
+   * Target to achieve.
+   */
+  private String target = "" ;
+  /**
+   *  Set the name of the Gantfile.
+   *
+   *  @param file The Gantfile.
+   */
+  public void setFile ( final String file ) { this.file = file ; }
+  /**
+   *  Set the target to be run.
+   *
+   *  @param target The target.
+   */
+  public void setTarget ( final String target ) { this.target = target ; }
+  /**
+   * Load the file and then execute it
+   */
+  public void execute ( ) throws BuildException {
+    if ( ! ( new File ( file ) ).exists ( ) ) { throw new BuildException ( "Gantfile does not exist." , getLocation ( ) ) ; }
+    final gant.Gant gant = new gant.Gant ( ) ;
+    final ArrayList parameters = new ArrayList ( ) ;
+    parameters.add ( "-f" ) ;
+    parameters.add ( file ) ;
+    if ( ! target.equals ( "" ) ) { parameters.add ( target ) ; }
+    if ( gant.process ( parameters ) != 0 ) { throw new BuildException ( "Gant execution failed." , getLocation ( ) ) ; }
+  }
+}
---
+++ src/test/groovy/org/codehaus/gant/ant/tests/Gant_Test.java
@@ -0,0 +1,75 @@
+//  Gant -- A Groovy build framework based on scripting Ant tasks.
+//
+//  Copyright © 2008 Russel Winder
+//
+//  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in
+//  compliance with the License. You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software distributed under the License is
+//  distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+//  implied. See the License for the specific language governing permissions and limitations under the
+//  License.
+
+package org.codehaus.gant.ant.tests ;
+
+import java.io.File ;
+
+import org.apache.tools.ant.BuildException ;
+import org.apache.tools.ant.Project ;
+import org.apache.tools.ant.ProjectHelper ;
+
+import groovy.util.GroovyTestCase ;
+
+/**
+ *  Unit tests for the Gant Ant task.
+ *
+ *  @author Russel Winder
+ */
+public class Gant_Test extends GroovyTestCase {
+  private final File antFile = new File ( "src/test/groovy/org/codehaus/gant/ant/tests/gantTest.xml" ) ;
+  private Project project ;
+
+  //  This variable is assigned in the Gant script hence the public static.
+  public static String returnValue ;
+  
+  protected void setUp ( ) throws Exception {
+    super.setUp ( ) ;
+    project = new Project ( ) ;
+    project.init ( ) ;
+    ProjectHelper.getProjectHelper ( ).parse ( project , antFile ) ;
+    returnValue = "" ;
+  }
+  
+  public void testDefaultTarget ( ) {
+    project.executeTarget ( "gantTestDefaultTarget" ) ;
+    assertEquals ( "A test target in the default file." , returnValue ) ;
+  }
+  
+  public void testNamedTarget ( ) {
+    project.executeTarget ( "gantTestNamedTarget" ) ;
+    assertEquals ( "Another target in the default file." , returnValue ) ;
+  }
+
+  public void testMissingGantfile ( ) {
+    try { project.executeTarget ( "missingGantfile" ) ; }
+    catch ( final BuildException be ) {
+      assertEquals ( "Gantfile does not exist." , be.getMessage ( ) ) ;
+      return ;
+    }
+    fail ( "Should have got a BuildException." ) ;
+  }
+
+  /**
+   *  Test the behaviour of a missing target in the Ant XML file.
+   */
+  public void testUnknownTarget ( ) {
+    try { project.executeTarget ( "blahBlahBlahBlah" ) ; }
+    catch ( final BuildException be ) {
+      assertEquals ( "Target \"blahBlahBlahBlah\" does not exist in the project \"testGantTask\". " , be.getMessage ( ) ) ;
+      return ;
+    }
+    fail ( "Should have got a BuildException." ) ;
+  }
+}
