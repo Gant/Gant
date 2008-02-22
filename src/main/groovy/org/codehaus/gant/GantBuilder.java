a63179cda137d309d517705db6fea2205c2c8110
russel <russel@d02f77da-47a5-474d-b610-f18e4bea325a>
2008-02-22 18:48:45 +0000

    r9242@balin:  russel | 2008-02-22 18:48:58 +0000  Add the -L|--lib option.  Started investigations of the Gant Ant Task classpath problem.
    git-svn-id: https://svn.codehaus.org/gant/gant/branches/1_1_X@9010 d02f77da-47a5-474d-b610-f18e4bea325a

--- src/main/groovy/org/codehaus/gant/GantBuilder.java
+++ src/main/groovy/org/codehaus/gant/GantBuilder.java
@@ -1,6 +1,6 @@
 //  Gant -- A Groovy build framework based on scripting Ant tasks.
 //
-//  Copyright © 2006-7 Russel Winder
+//  Copyright © 2006-8 Russel Winder
 //
 //  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in
 //  compliance with the License. You may obtain a copy of the License at
@@ -20,13 +20,27 @@ import java.util.Map ;
 import groovy.lang.Closure ;
 import groovy.util.AntBuilder ;
 
+import org.apache.tools.ant.Project ;
+
 /**
- *  This class is a sub-class of <code>AntBuilder</code> to provide dry-run capability and to deal with all
- *  the verbosity issues.
+ *  This class is a sub-class of <code>AntBuilder</code> to provide extra capabilities.  In particular, a
+ *  dry-run capability, and the implementation of various levels of verbosity.
+ *
+ *  <p>If execution is from a command line Gant or call from a Groovy script then the class loader for all
+ *  objects is a single instance of <code>org.codehaus.groovy.tools.RootLoader</code>, which already has Ant
+ *  and Groovy jars in the classpath.  If however execution is from an Ant execution via the Gant Ant Task,
+ *  then the classloader for the instance is an instance of <code>org.apache.tools.ant.AntClassLoader</code>
+ *  with Ant and Groovy jars on the classpath BUT the class loader for the
+ *  <code>org.apache.tools.ant.Project</code> instance is a simple <code>java.net.URLClassLoader</code> and
+ *  does not have the necessary jars on the classpath.  When using Ant, the Ant jar has been loaded before
+ *  the Groovy aspects of the classpath have been set up.  So we must allow for a specialized constructor
+ *  taking a preprepared <code>org.apache.tools.ant.Project</code> to handle this situation. </p>
  *
  *  @author Russel Winder <russel.winder@concertant.com>
  */
 public class GantBuilder extends AntBuilder {
+  public GantBuilder ( ) { }
+  public GantBuilder ( final Project project ) { super ( project ) ; }
   public Object invokeMethod ( final String name , final Object arguments ) {
     if ( GantState.dryRun ) {
       if ( GantState.verbosity > GantState.SILENT ) {
