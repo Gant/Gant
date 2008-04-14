811f0b029076b7ffdea819dfc0187c2a5d62656b
russel <russel@d02f77da-47a5-474d-b610-f18e4bea325a>
2008-04-14 17:22:50 +0000

    Make no assumptions about name lookup, force items to be set in the object removing the risk of setting in the global binding.
    git-svn-id: https://svn.codehaus.org/gant/gant/trunk@9080 d02f77da-47a5-474d-b610-f18e4bea325a

--- src/main/groovy/org/codehaus/gant/GantBinding.groovy
+++ src/main/groovy/org/codehaus/gant/GantBinding.groovy
@@ -42,73 +42,73 @@ public class GantBinding extends Binding implements Cloneable {
     if ( classLoader.class.name == "org.apache.tools.ant.AntClassLoader" ) {
       //final project = classLoader.forceLoadClass ( 'org.apache.tools.ant.Project' ).newInstance ( )
       //project.init ( )
-      //ant = new GantBuilder ( project )
-      ant = new GantBuilder ( )
+      //setVariable ( ant , new GantBuilder ( project ) )
+      setVariable ( 'ant' , new GantBuilder ( ) )
     }
-    else { ant = new GantBuilder ( ) } 
+    else { setVariable ( 'ant' , new GantBuilder ( ) ) } 
     ant.property ( environment : 'environment' )
     //  Ensure Ant as well as ant is available to ensure backward compatibility.
-    Ant = ant
-    includeTargets = new IncludeTargets ( this )
-    includeTool = new IncludeTool ( this )
-    target = { Map map , Closure closure ->
-      switch ( map.size ( ) ) {
-       case 0 : throw new RuntimeException ( 'Target specified without a name.' )
-       case 1 : break
-       default : throw new RuntimeException ( 'Target specified with multiple names.' )
-      }
-      def targetName = map.keySet ( ).iterator ( ).next ( )
-      try {
-        owner.getVariable ( targetName )
-        //
-        //  Exceptions thrown in this Closure appear not to cause execution to enter an error path.  Must
-        //  find out how to throw an exception from a Closure.
-        //
-        //throw new RuntimeException ( "Attempt to redefine " + targetName )
-        //
-        System.err.println ( 'Warning, target causing name overwriting of name ' + targetName )
-        //System.exit ( 101 ) 
-      }
-      catch ( MissingPropertyException ) { }         
-      def targetDescription = map.get ( targetName )
-      if ( targetDescription ) { targetDescriptions.put ( targetName , targetDescription ) }
-      closure.metaClass = new GantMetaClass ( closure.class , owner )
-      owner.setVariable ( targetName , closure )
-      owner.setVariable ( targetName + '_description' , targetDescription )
-    }
-    task = { Map map , Closure closure ->
-      System.err.println ( 'Deprecation warning: Use of task instead of target is deprecated and will be removed in version 1.3.' )
-      target ( map , closure )
-    }
-    targetDescriptions = new TreeMap ( )
-    message = { String tag , Object message ->
-      def padding = 9 - tag.length ( )
-      if ( padding < 0 ) { padding = 0 }
-      println ( "           ".substring ( 0 , padding ) + '[' + tag + '] ' + message )
-    }
-    setDefaultTarget = { defaultTarget -> // Deal with Closure or String arguments.
-      switch ( defaultTarget.getClass ( ) ) {
-       case Closure :
-        def targetName = null
-        owner.variables.each { key , value -> if ( value.is ( defaultTarget ) ) { targetName = key } }
-        if ( targetName == null ) { throw new RuntimeException ( 'Parameter to setDefaultTarget method is not a known target.  This can never happen!' ) }
-        owner.target.call ( 'default' : targetName ) { defaultTarget ( ) }
-        break
-       case String :
-        def failed = true
-        try {
-          def targetClosure = owner.getVariable ( defaultTarget )
-          if ( targetClosure != null ) { owner.target.call ( 'default' : defaultTarget ) { targetClosure ( ) } ; failed = false }
+    setVariable ( 'Ant' , getVariable ( 'ant' ) )
+    setVariable ( 'includeTargets' , new IncludeTargets ( this ) )
+    setVariable ( 'includeTool' , new IncludeTool ( this ) )
+    setVariable ( 'target' , { Map map , Closure closure ->
+        switch ( map.size ( ) ) {
+         case 0 : throw new RuntimeException ( 'Target specified without a name.' )
+         case 1 : break
+         default : throw new RuntimeException ( 'Target specified with multiple names.' )
         }
-        catch ( MissingPropertyException mpe ) { }
-        if ( failed ) { throw new RuntimeException ( "Target ${defaultTarget} does not exist so cannot be made the default." ) }
-        break
-       default :
-        throw new RuntimeException ( 'Parameter to setDefaultTarget is of the wrong type -- must be a target reference or a string.' )
-        break 
-      }
-    }
-    cacheEnabled = false
+        def targetName = map.keySet ( ).iterator ( ).next ( )
+        try {
+          owner.getVariable ( targetName )
+          //
+          //  Exceptions thrown in this Closure appear not to cause execution to enter an error path.  Must
+          //  find out how to throw an exception from a Closure.
+          //
+          //throw new RuntimeException ( "Attempt to redefine " + targetName )
+          //
+          System.err.println ( 'Warning, target causing name overwriting of name ' + targetName )
+          //System.exit ( 101 ) 
+        }
+        catch ( MissingPropertyException ) { }         
+        def targetDescription = map.get ( targetName )
+        if ( targetDescription ) { targetDescriptions.put ( targetName , targetDescription ) }
+        closure.metaClass = new GantMetaClass ( closure.class , owner )
+        owner.setVariable ( targetName , closure )
+        owner.setVariable ( targetName + '_description' , targetDescription )
+      } )
+    setVariable ( 'task' , { Map map , Closure closure ->
+        System.err.println ( 'Deprecation warning: Use of task instead of target is deprecated and will be removed in version 1.3.' )
+        target ( map , closure )
+      } )
+    setVariable ( 'targetDescriptions' , new TreeMap ( ) )
+    setVariable ( 'message' , { String tag , Object message ->
+        def padding = 9 - tag.length ( )
+        if ( padding < 0 ) { padding = 0 }
+        println ( "           ".substring ( 0 , padding ) + '[' + tag + '] ' + message )
+      } )
+    setVariable ( 'setDefaultTarget' , { defaultTarget -> // Deal with Closure or String arguments.
+         switch ( defaultTarget.getClass ( ) ) {
+          case Closure :
+          def targetName = null
+          owner.variables.each { key , value -> if ( value.is ( defaultTarget ) ) { targetName = key } }
+          if ( targetName == null ) { throw new RuntimeException ( 'Parameter to setDefaultTarget method is not a known target.  This can never happen!' ) }
+          owner.target.call ( 'default' : targetName ) { defaultTarget ( ) }
+          break
+          case String :
+          def failed = true
+          try {
+            def targetClosure = owner.getVariable ( defaultTarget )
+            if ( targetClosure != null ) { owner.target.call ( 'default' : defaultTarget ) { targetClosure ( ) } ; failed = false }
+          }
+          catch ( MissingPropertyException mpe ) { }
+          if ( failed ) { throw new RuntimeException ( "Target ${defaultTarget} does not exist so cannot be made the default." ) }
+          break
+          default :
+          throw new RuntimeException ( 'Parameter to setDefaultTarget is of the wrong type -- must be a target reference or a string.' )
+          break 
+         }
+      } )
+    setVariable ( 'cacheEnabled' , false )
     def item = System.getenv ( ).GANTLIB ;
     if ( item == null ) { gantLib = [ ] }
     else { gantLib = Arrays.asList ( item.split ( System.properties.'path.separator' ) ) }
