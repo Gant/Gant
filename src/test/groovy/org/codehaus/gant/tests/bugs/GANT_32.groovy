a04115f2aea304ddedb8bf15b023ecc9fc9ec868
russel <russel@d02f77da-47a5-474d-b610-f18e4bea325a>
2008-06-28 07:22:33 +0000

    Add the licence header file to GANT_32.groovy that should have been there in the first place.
    git-svn-id: https://svn.codehaus.org/gant/gant/trunk@9457 d02f77da-47a5-474d-b610-f18e4bea325a

--- src/test/groovy/org/codehaus/gant/tests/bugs/GANT_32.groovy
+++ src/test/groovy/org/codehaus/gant/tests/bugs/GANT_32.groovy
@@ -1,3 +1,17 @@
+//  Gant -- A Groovy build framework based on scripting Ant tasks.
+//
+//  Copyright © 2008 Russel Winder
+//
+//  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in
+//  compliance with the License. You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software distributed under the License is
+//  distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+//  implied. See the License for the specific language governing permissions and limitations under the
+//  License.
+
 package org.codehaus.gant.tests.bugs
 
 import org.codehaus.gant.tests.GantTestCase
