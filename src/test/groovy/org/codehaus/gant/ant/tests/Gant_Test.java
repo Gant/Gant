2504e1cc0e15809ea56811b2ba7ed87a2f5b83f4
russel <russel@d02f77da-47a5-474d-b610-f18e4bea325a>
2008-07-11 14:18:03 +0000

    Amend the way the pipe from the process is read so as to avoid pipe full issues. This change was made to test whether the problem was pipe related but it wasn't. Keep the change anyway as it is better to take things out of the pipe as soon as possible.
    git-svn-id: https://svn.codehaus.org/gant/gant/trunk@9566 d02f77da-47a5-474d-b610-f18e4bea325a

--- src/test/groovy/org/codehaus/gant/ant/tests/Gant_Test.java
+++ src/test/groovy/org/codehaus/gant/ant/tests/Gant_Test.java
@@ -103,25 +103,44 @@ public class Gant_Test extends TestCase {
     //    [gant] 1 error
     //    [gant] 
     //
+    //  If Gant is compiled against Groovy 1.5.6 or 1.6-beta-2 then it succeeds fine.  If Gant is compiled
+    //  against Groovy 1.6-beta-1 then there is an infinite recursion in the metaclass system emanating from
+    //  InvokerHelper as the Gant Ant task is started leading to a StackOverflowError, and so the correct
+    //  output is not received.
+    //
     final ProcessBuilder pb = new ProcessBuilder ( "ant" , "-f" , "src/test/groovy/org/codehaus/gant/ant/tests/gantTest.xml" ) ;
     try {
-      final Process p = pb.start ( ) ;
-      try { assertEquals ( 1 , p.waitFor ( ) ) ; }
-      catch ( final InterruptedException ie ) { fail ( "Got an InterruptedException which should not have happened." ) ; }
       final StringBuilder sb = new StringBuilder ( ) ;
+      final Process p = pb.start ( ) ;  //  Could throw an IOException hence the try block.
       final BufferedReader br = new BufferedReader ( new InputStreamReader ( p.getInputStream ( ) ) ) ;
-      while ( true ) {
-        final String line = br.readLine ( ) ;
-        if ( line == null ) { break ; }
-        sb.append ( line ) ;
-        sb.append ( System.getProperty ( "line.separator" ) ) ;
-      }
+      final Thread readThread = new Thread ( ) {
+          public void run ( ) {
+            try {
+              while ( true ) {
+                final String line = br.readLine ( ) ;  //  Could throw an IOException hence the try block.
+
+                System.err.println ( line ) ;
+                
+                if ( line == null ) { break ; }
+                sb.append ( line ) ;
+                sb.append ( System.getProperty ( "line.separator" ) ) ;
+              }
+            }
+            catch ( final IOException ioe ) { fail ( "Got an IOException reading a line in the read thread." ) ; }
+          }
+        } ;
+      readThread.start ( ) ;
+      try { assertEquals ( 1 , p.waitFor ( ) ) ; }
+      catch ( final InterruptedException ie ) { fail ( "Got an InterruptedException waiting for the Ant process to finish." ) ; }
+      try { readThread.join ( ) ;}
+      catch ( final InterruptedException ie ) { fail ( "Got an InterruptedException waiting for the read thread to terminate." ) ; }
+      
       /*
        *  Temporarily remove this test whilst investigating why it works generally but not in certain circumstances.
        *
       assertEquals ( "Buildfile: src/test/groovy/org/codehaus/gant/ant/tests/gantTest.xml\n\n-initializationWithGroovyHome:\n\n-initializationOtherwise:\n\ngantTestDefaultTarget:\n     [gant] Error evaluating Gantfile: startup failed, build_gant: 15: unable to resolve class org.codehaus.gant.ant.tests.Gant_Test\n     [gant]  @ line 15, column 1.\n     [gant] 1 error\n     [gant] \n", sb.toString ( ) ) ;
       */
     }
-    catch ( final IOException ioe ) { fail ( "Got an IOException which should not have happened." ) ; }
+    catch ( final IOException ioe ) { fail ( "Got an IOException from starting the process." ) ; }
   }
 }
