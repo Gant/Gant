38359f1a7593192795d61be2614591a152ed7c49
Russel Winder <russel@winder.org.uk>
2014-10-28 22:34:17 +0000

    Tinker with the Ivy example to update things.

--- examples/ivy/source/example/Hello.java
+++ examples/ivy/source/example/Hello.java
@@ -1,6 +1,6 @@
-//   Gant -- A Groovy way of scripting Ant tasks.
+//   Gant — A Groovy way of scripting Ant tasks.
 //
-//  Copyright © 2006–2008, 2010, 2013  Russel Winder
+//  Copyright © 2006–2008, 2010, 2013, 2014  Russel Winder
 //
 //  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in
 //  compliance with the License. You may obtain a copy of the License at
@@ -20,8 +20,8 @@ import org.apache.commons.lang.WordUtils;
 
 public class Hello {
   public static void main(final String[] args) {
-    String  message = "hello ivy !";
-    System.out.println("Standard message : " + message);
-    System.out.println("Capitalized by " + WordUtils.class.getName()  + " : " + WordUtils.capitalizeFully(message));
+    String  message = "hello ivy!";
+    System.out.println("Standard message: " + message);
+    System.out.println("Capitalized by " + WordUtils.class.getName()  + ": " + WordUtils.capitalizeFully(message));
   }
 }
